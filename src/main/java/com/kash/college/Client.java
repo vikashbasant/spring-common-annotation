fb0d0245ca77c46b011b22729b676bb9809a449f
Vikash Basant <basantvikash360@gmail.com>
2023-07-05 18:03:59 +0530

    doing configuration in the java class insteaded of bean.xml file

--- src/main/java/com/kash/college/Client.java
+++ src/main/java/com/kash/college/Client.java
@@ -1,17 +1,18 @@
 package com.kash.college;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Client {
 
     public static void main(String[] args) {
 
-        // Creating an object using beans.xml file
-        // inside the beans.xml file, we need to specify the class which we want an object:
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
-        System.out.println("=>>beans.xml file config loaded!<<=");
+        // Create the object CollegeConfig class from base package:
+        // Register that object in the application context inside the IOC container:
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(CollegeConfig.class);
 
-        // Get Beans with id="student" from beans.xml file:
+
+        // Fetch the object through bean id="collegeBean"
         College college = context.getBean("collegeBean", College.class);
         System.out.println(college);
 
---
+++ src/main/java/com/kash/college/CollegeConfig.java
@@ -0,0 +1,10 @@
+package com.kash.college;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.kash.college")
+public class CollegeConfig {
+
+}
